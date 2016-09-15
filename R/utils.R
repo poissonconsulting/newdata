ed6d245253e9598e0d88ad3c5ab49085df2ba876
Joe Thorley <joe@poissonconsulting.ca>
2016-09-15 11:39:28 -0700

    Test classes and is.named

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,3 @@
-classes <- function(x) vapply(x, class, "")
+classes <- function(x) lapply(x, class)
 is.named <- function(x) !is.null(names(x))
 factor_with_length_greater_than_1 <- function(x) is.factor(x) && length(x) > 1
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,5 +1,15 @@
 context("utils")
 
+test_that("classes", {
+  expect_identical(classes(Orange), list(Tree = c("ordered", "factor"), age = "numeric",
+                                         circumference = "numeric"))
+})
+
+test_that("is.named", {
+  expect_true(is.named(Orange))
+  expect_false(is.named(list(1)))
+})
+
 test_that("factor_with_length_greater_than_1", {
   expect_false(factor_with_length_greater_than_1(1))
   expect_false(factor_with_length_greater_than_1(1:2))
