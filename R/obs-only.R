7a15874d9f4639a8498703fc4a8e284f2936ec17
Joe Thorley <joe@poissonconsulting.ca>
2023-09-24 07:33:37 -0700

    refactor obs_only

--- R/obs-only.R
+++ R/obs-only.R
@@ -1,7 +1,7 @@
 obs_only <- function(new_data, data, obo) {
   data <- data[names(data) %in% obo]
-  if (ncol(data)) {
-    new_data <- new_data %>% merge(unique(data), by = obo)
+  if (!ncol(data)) {
+    return(new_data)
   }
-  new_data
+  new_data %>% merge(unique(data), by = obo)
 }
