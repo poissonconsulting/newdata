1b4d032e688ef9bf978177da46177e27aeddc3b7
Joe Thorley <joe@poissonconsulting.ca>
2023-09-26 10:13:53 -0700

    revamp new value tests

--- tests/testthat/test-new-value.R
+++ tests/testthat/test-new-value.R
@@ -1,144 +1,234 @@
 test_that("new_value logical", {
+  # zero length
   expect_identical(new_value(logical(0)), FALSE)
+  # missing value
   expect_identical(new_value(NA), FALSE)
+  # single value
   expect_identical(new_value(FALSE), FALSE)
   expect_identical(new_value(TRUE), FALSE)
+  # multiple values
   expect_identical(new_value(c(TRUE, FALSE)), FALSE)
+  expect_identical(new_value(c(FALSE, TRUE)), FALSE)
+  expect_identical(new_value(c(TRUE, TRUE)), FALSE)
+  expect_identical(new_value(c(TRUE, TRUE, FALSE)), FALSE)
+  # multiple values with missing
   expect_identical(new_value(c(TRUE, FALSE, NA)), FALSE)
+  expect_identical(new_value(c(FALSE, TRUE, NA)), FALSE)
   expect_identical(new_value(c(TRUE, TRUE, NA)), FALSE)
+  expect_identical(new_value(c(TRUE, TRUE, FALSE, NA)), FALSE)
 })
 
-# should make independent of locale!!
+test_that("new_value integer", {
+  # zero length
+  expect_identical(new_value(integer()), NA_integer_)
+  # missing value
+  expect_identical(new_value(NA_integer_), NA_integer_)
+  # single value
+  expect_identical(new_value(1L), 1L)
+  expect_identical(new_value(10L), 10L)
+  expect_identical(new_value(-1L), -1L)
+  expect_identical(new_value(0L), 0L)
+  # multiple values
+  expect_identical(new_value(0:1), 0L)
+  expect_identical(new_value(1:2), 2L)
+  expect_identical(new_value(c(1L, 3L)), 2L)
+  # multiple values with missing
+  expect_identical(new_value(c(0:1, NA)), 0L)
+  expect_identical(new_value(c(1:2, NA)), 2L)
+  expect_identical(new_value(c(1L, 3L, NA)), 2L)
+})
+
+test_that("new_value real", {
+  # zero length
+  expect_identical(new_value(double()), NA_real_)
+  # missing value
+  expect_identical(new_value(NA_real_), NA_real_)
+  # single value
+  expect_identical(new_value(1), 1)
+  expect_identical(new_value(1.1), 1.1)
+  expect_identical(new_value(10), 10)
+  expect_identical(new_value(-1), -1)
+  expect_identical(new_value(0), 0)
+  # multiple values
+  expect_identical(new_value(c(0, 1)), 0.5)
+  expect_identical(new_value(c(1, 2)), 1.5)
+  expect_identical(new_value(c(1, 3)), 2)
+  # multiple values with missing
+  expect_identical(new_value(c(0, 1, NA)), 0.5)
+  expect_identical(new_value(c(1, 2, NA)), 1.5)
+  expect_identical(new_value(c(1, 3, NA)), 2)
+})
+
+# FIXME should make independent of locale!!
 test_that("new_value character", {
+  # zero length
   expect_identical(new_value(character()), NA_character_)
+  # missing value
   expect_identical(new_value(NA_character_), NA_character_)
+  # single value
   expect_identical(new_value("b"), "b")
+  # multiple values
   expect_identical(new_value(c("a", "b")), "a")
   expect_identical(new_value(c("b", "a")), "a")
   expect_identical(new_value(c("b", "b", "a")), "a")
-  expect_identical(new_value(c(NA, "b", "b", "a")), "a")
+  # multiple values with missing
+  expect_identical(new_value(c("a", "b", NA)), "a")
+  expect_identical(new_value(c("b", "a", NA)), "a")
+  expect_identical(new_value(c("b", "b", "a", NA)), "a")
 })
 
 test_that("new_value factor", {
-#  expect_identical(new_value(factor()), NA_) factor no length
+  # zero length
+  expect_identical(new_value(factor()), factor(NA))
+  # missing value
   expect_identical(new_value(factor(NA)), factor(NA))
-  expect_identical(new_value(factor("b")), factor("b"))
-  expect_identical(new_value(factor(c("b", NA))), factor("b"))
-  expect_identical(new_value(factor(c("a", "b"))), factor("a", levels = c("a", "b")))
-  expect_identical(new_value(factor(c("a", "b"), levels = c("b", "a"))), factor("b", levels = c("b", "a")))
-  expect_identical(new_value(factor(c("b", "a"), levels = c("b", "a"))), factor("b", levels = c("b", "a")))
-  expect_identical(new_value(factor(c("c", "b", "a"), levels = c("b", "c", "a"))), factor("b", levels = c("b", "c", "a")))
-  expect_identical(new_value(factor(c("a", "b", "a"), levels = c("b", "a"))), factor("b", levels = c("b", "a")))
-  expect_identical(new_value(factor(c("a", "b", "a"), levels = c("b", "a"))), factor("b", levels = c("b", "a")))
+  "single value"
+  expect_identical(new_value(factor("b", levels = "b")),
+                   factor("b", levels = "b"))
+  expect_identical(new_value(factor("b", levels = c("a", "b"))),
+                   factor("a", levels = c("a", "b")))
+  expect_identical(new_value(factor("b", levels = c("b", "a"))),
+                   factor("b", levels = c("b", "a")))
+  expect_identical(new_value(factor("a", levels = c("b", "a"))),
+                   factor("b", levels = c("b", "a")))
+  # multiple values
+  expect_identical(new_value(factor(c("a", "b"))),
+                   factor("a", levels = c("a", "b")))
+  expect_identical(new_value(factor(c("a", "b"), levels = c("b", "a"))),
+                   factor("b", levels = c("b", "a")))
+  expect_identical(new_value(factor(c("b", "a"), levels = c("b", "a"))),
+                   factor("b", levels = c("b", "a")))
+  expect_identical(new_value(factor(c("c", "b", "a"), levels = c("b", "c", "a"))),
+                   factor("b", levels = c("b", "c", "a")))
+  expect_identical(new_value(factor(c("a", "b", "a"), levels = c("b", "a"))),
+                   factor("b", levels = c("b", "a")))
+  expect_identical(new_value(factor(c("a", "b", "a"), levels = c("b", "a"))),
+                   factor("b", levels = c("b", "a")))
+  # multiple values missing
+  expect_identical(new_value(factor(c("a", "b", NA))),
+                   factor("a", levels = c("a", "b")))
+  expect_identical(new_value(factor(c("a", "b", NA), levels = c("b", "a"))),
+                   factor("b", levels = c("b", "a")))
+  expect_identical(new_value(factor(c("b", "a", NA), levels = c("b", "a"))),
+                   factor("b", levels = c("b", "a")))
+  expect_identical(new_value(factor(c("c", "b", "a", NA), levels = c("b", "c", "a"))),
+                   factor("b", levels = c("b", "c", "a")))
+  expect_identical(new_value(factor(c("a", "b", "a", NA), levels = c("b", "a"))),
+                   factor("b", levels = c("b", "a")))
+  expect_identical(new_value(factor(c("a", "b", "a", NA), levels = c("b", "a"))),
+                   factor("b", levels = c("b", "a")))
 })
 
-test_that("new_value scalar", {
-  dinteger <- 2L
-  dnumeric <- 1.1
-  dcharacter <- "1"
-  dfactor <- factor(1)
-  dordered <- ordered(1)
-  ddate <- as.Date("2000-01-01")
-  dposix <- ISOdate(2000, 1, 1, 12, tz = "PST8PDT")
-  dhms <- as_hms("10:00:00")
-  default <- complex(real = 1.1)
-
-  expect_identical(new_value(dinteger), dinteger)
-  expect_equal(new_value(dnumeric), dnumeric)
-  expect_identical(new_value(dcharacter), dcharacter)
-  expect_identical(new_value(dfactor), dfactor)
-  expect_identical(new_value(dordered), dordered)
-  expect_identical(new_value(ddate), ddate)
-  expect_identical(new_value(dposix), dposix)
-  expect_identical(new_value(dhms), dhms)
-  expect_equal(new_value(default), default)
+test_that("new_value ordered", {
+  # zero length
+  expect_identical(new_value(ordered()), ordered(NA))
+  # missing value
+  expect_identical(new_value(ordered(NA)), ordered(NA))
+  "single value"
+  expect_identical(new_value(ordered("b", levels = "b")),
+                   ordered("b", levels = "b"))
+  expect_identical(new_value(ordered("b", levels = c("a", "b"))),
+                   ordered("a", levels = c("a", "b")))
+  expect_identical(new_value(ordered("b", levels = c("b", "a"))),
+                   ordered("b", levels = c("b", "a")))
+  expect_identical(new_value(ordered("a", levels = c("b", "a"))),
+                   ordered("b", levels = c("b", "a")))
+  # multiple values
+  expect_identical(new_value(ordered(c("a", "b"))),
+                   ordered("a", levels = c("a", "b")))
+  expect_identical(new_value(ordered(c("a", "b"), levels = c("b", "a"))),
+                   ordered("b", levels = c("b", "a")))
+  expect_identical(new_value(ordered(c("b", "a"), levels = c("b", "a"))),
+                   ordered("b", levels = c("b", "a")))
+  expect_identical(new_value(ordered(c("c", "b", "a"), levels = c("b", "c", "a"))),
+                   ordered("b", levels = c("b", "c", "a")))
+  expect_identical(new_value(ordered(c("a", "b", "a"), levels = c("b", "a"))),
+                   ordered("b", levels = c("b", "a")))
+  expect_identical(new_value(ordered(c("a", "b", "a"), levels = c("b", "a"))),
+                   ordered("b", levels = c("b", "a")))
+  # multiple values missing
+  expect_identical(new_value(ordered(c("a", "b", NA))),
+                   ordered("a", levels = c("a", "b")))
+  expect_identical(new_value(ordered(c("a", "b", NA), levels = c("b", "a"))),
+                   ordered("b", levels = c("b", "a")))
+  expect_identical(new_value(ordered(c("b", "a", NA), levels = c("b", "a"))),
+                   ordered("b", levels = c("b", "a")))
+  expect_identical(new_value(ordered(c("c", "b", "a", NA), levels = c("b", "c", "a"))),
+                   ordered("b", levels = c("b", "c", "a")))
+  expect_identical(new_value(ordered(c("a", "b", "a", NA), levels = c("b", "a"))),
+                   ordered("b", levels = c("b", "a")))
+  expect_identical(new_value(ordered(c("a", "b", "a", NA), levels = c("b", "a"))),
+                   ordered("b", levels = c("b", "a")))
 })
 
-test_that("new_value vector", {
-  dinteger <- 1:10
-  dnumeric <- 1:10 + 0.1
-  dcharacter <- as.character(1:10)
-  dfactor <- factor(1:10)
-  ddate <- as.Date("2000-01-01") + 1:10
-  dposix <- ISOdate(2000, 1, 1, 12, tz = "PST8PDT") + 1:10
-  dhms <- as_hms(as_hms("10:00:00") + 1:10)
-  default <- complex(real = 1:10 + 0.1)
-
-  expect_identical(new_value(c(TRUE, FALSE)), FALSE)
-  expect_identical(new_value(dinteger), 6L)
-  expect_equal(new_value(dnumeric), 5.6)
-  expect_identical(new_value(dcharacter), "1")
-  expect_identical(new_value(rev(dcharacter)), "1")
-  expect_identical(new_value(dfactor), dfactor[1])
-  expect_identical(new_value(rev(dfactor)), dfactor[1])
-  expect_identical(new_value(ddate), as.Date("2000-01-06"))
-  expect_identical(
-    new_value(dposix),
-    ISOdate(2000, 1, 1, 12, 0, 6, tz = "PST8PDT")
-  )
-  expect_identical(new_value(dhms), as_hms("10:00:06"))
-  expect_equal(new_value(default), complex(real = 5.6))
+test_that("new_value Date", {
+  # zero length
+#  expect_identical(new_value(as.Date(character())), as.Date(NA_character_)) # FIXME
+  # missing value
+#  expect_identical(new_value(as.Date(NA_character_)), as.Date(NA_character_)) # FIXME
+  # single value
+#  expect_identical(new_value(as.Date(1L)), as.Date(1L)) # FIXME
+  expect_identical(new_value(as.Date(1)), as.Date(1))
+  expect_identical(new_value(as.Date(1.1)), as.Date(1))
+  expect_identical(new_value(as.Date(1.6)), as.Date(2))
+  expect_identical(new_value(as.Date(10)), as.Date(10))
+  # multiple values
+  expect_identical(new_value(as.Date(c(0, 1))), as.Date(0))
+  expect_identical(new_value(as.Date(c(1, 2))), as.Date(2))
+  expect_identical(new_value(as.Date(c(1, 3))), as.Date(2))
+  # multiple values with missing
+  expect_identical(new_value(as.Date(c(0, 1, NA))), as.Date(0))
+  expect_identical(new_value(as.Date(c(1, 2, NA))), as.Date(2))
+  expect_identical(new_value(as.Date(c(1, 3, NA))), as.Date(2))
 })
 
-test_that("new_value scalar NA", {
-  dinteger <- as.integer(NA)
-  dnumeric <- as.numeric(NA)
-  dcharacter <- as.character(NA)
-  dfactor <- factor(NA)
-  ddate <- as.Date(NA)
-  dposix <- as.POSIXct(NA, tz = "PST8PDT")
-  dhms <- as_hms(NA)
-  default <- complex(real = NA)
-
-  expect_identical(new_value(dinteger), dinteger)
-  expect_equal(new_value(dnumeric), dnumeric)
-  expect_identical(new_value(dcharacter), dcharacter)
-  expect_identical(new_value(dfactor), dfactor)
-  expect_equal(new_value(ddate), ddate)
-  # FIXME when go to R 4.1
-  expect_equal(new_value(dposix), dposix, ignore_attr = TRUE)
-  expect_equal(new_value(dhms), dhms)
-#  expect_equal(new_value(default), default) #FIXME as an example
+test_that("new_value POSIXct", {
+  # zero length
+ # expect_identical(new_value(as.POSIXct(character())), as.POSIXct(NA_character_)) # FIXME
+  # missing value
+  #  expect_identical(new_value(as.POSIXct(NA_character_)), as.POSIXct(NA_character_)) # FIXME
+  # single value
+  #  expect_identical(new_value(as.POSIXct(1L)), as.POSIXct(1L)) # FIXME
+  # expect_identical(new_value(as.POSIXct(1)), as.POSIXct(1))
+  # expect_identical(new_value(as.POSIXct(1.1)), as.POSIXct(1))
+  # expect_identical(new_value(as.POSIXct(1.6)), as.POSIXct(2))
+  # expect_identical(new_value(as.POSIXct(10)), as.POSIXct(10))
+  # # multiple values
+  # expect_identical(new_value(as.POSIXct(c(0, 1))), as.POSIXct(0))
+  # expect_identical(new_value(as.POSIXct(c(1, 2))), as.POSIXct(2))
+  # expect_identical(new_value(as.POSIXct(c(1, 3))), as.POSIXct(2))
+  # # multiple values with missing
+  # expect_identical(new_value(as.POSIXct(c(0, 1, NA))), as.POSIXct(0))
+  # expect_identical(new_value(as.POSIXct(c(1, 2, NA))), as.POSIXct(2))
+  # expect_identical(new_value(as.POSIXct(c(1, 3, NA))), as.POSIXct(2))
 })
 
-
-test_that("new_value date and time rounding", {
-  ddate <- as.Date("2000-01-01") + 0.9
-  dposix <- ISOdate(2000, 1, 1, 12, 0, 0, tz = "PST8PDT") + 0.9
-  dhms <- as_hms(as_hms("10:00:00") + 0.9)
-  dhms2 <- as_hms(as_hms("23:59:59") + 0.9)
-
-  expect_identical(new_value(ddate), as.Date("2000-01-02"))
-  expect_identical(
-    new_value(dposix),
-    ISOdate(2000, 1, 1, 12, 0, 1, tz = "PST8PDT")
-  )
-  expect_identical(new_value(dhms), as_hms("10:00:01"))
-  expect_identical(new_value(dhms2), as_hms("00:00:00"))
+test_that("new_value hms", {
+  # zero length
+  expect_identical(new_value(as_hms(character())), as_hms(NA_character_))
+  # missing value
+  expect_identical(new_value(as_hms(NA_character_)), as_hms(NA_character_))
+  # single value
+  expect_identical(new_value(as_hms(1L)), as_hms(1L))
+  expect_identical(new_value(as_hms(1)), as_hms(1))
+  expect_identical(new_value(as_hms(1.1)), as_hms(1))
+  expect_identical(new_value(as_hms(1.6)), as_hms(2))
+  expect_identical(new_value(as_hms(10)), as_hms(10))
+  # multiple values
+  expect_identical(new_value(as_hms(c(0, 1))), as_hms(0))
+  expect_identical(new_value(as_hms(c(1, 2))), as_hms(2))
+  expect_identical(new_value(as_hms(c(1, 3))), as_hms(2))
+  expect_identical(new_value(as_hms(c("23:59:59", "00:00:02"))), as_hms("12:00:00"))
+  # multiple values with missing
+  expect_identical(new_value(as_hms(c(0, 1, NA))), as_hms(0))
+  expect_identical(new_value(as_hms(c(1, 2, NA))), as_hms(2))
+  expect_identical(new_value(as_hms(c(1, 3, NA))), as_hms(2))
 })
 
-test_that("new_value with missing", {
-  dinteger <- c(1:10, NA)
-  dnumeric <- c(1:10 + 0.1, NA)
-  dcharacter <- c(as.character(1:10), NA)
-  dfactor <- factor(c(1:10, NA))
-  ddate <- c(as.Date("2000-01-01") + 1:10, NA)
-  dposix <- ISOdate(2000, 1, 1, 12, tz = "PST8PDT") + c(1:10, NA)
-  dhms <- as_hms(as_hms("10:00:00") + c(1:10, NA))
-  default <- complex(real = c(1:10 + 0.1, NA))
-
-  expect_identical(new_value(c(TRUE, FALSE)), FALSE)
-  expect_identical(new_value(dinteger), 6L)
-  expect_equal(new_value(dnumeric), 5.6)
-  expect_identical(new_value(dcharacter), "1")
-  expect_identical(new_value(rev(dcharacter)), "1")
-  expect_identical(new_value(dfactor), dfactor[1])
-  expect_identical(new_value(rev(dfactor)), dfactor[1])
-  expect_identical(new_value(ddate), as.Date("2000-01-06"))
-  expect_identical(
-    new_value(dposix),
-    ISOdate(2000, 1, 1, 12, 0, 6, tz = "PST8PDT")
-  )
-  expect_identical(new_value(dhms), as_hms("10:00:06"))
-  expect_equal(new_value(default), complex(real = 5.6))
+test_that("new_value default", {
+  # zero length
+  # missing value
+  # single value
+  # multiple values
+  # multiple values with missing
 })
