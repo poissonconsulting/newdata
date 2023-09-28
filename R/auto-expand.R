1443686d56d093032029437f558857f0ed9c4ff3
Joe Thorley <joe@poissonconsulting.ca>
2023-09-28 16:45:13 -0700

    autoexpand script

---
+++ R/auto-expand.R
@@ -0,0 +1,53 @@
+map <- function(.x, .f, ...) {
+  .f <- as_function(.f, env = global_env())
+  lapply(.x, .f, ...)
+}
+
+new_tseq <- function(x, ...) {
+  expr <- ensym(x)
+  out <- new_seq(x, ...)
+  tibble::tibble(!!expr := out)
+}
+
+expand_defaults <- function(data, quos, default) {
+  default <- as_function(default)
+  ptype <- tidyr::expand(data[0, ], !!!quos)
+
+  default_names <- setdiff(names(data), names(ptype))
+  default_values <- map(data[default_names], default)
+
+  quo(tidyr::nesting(tibble::tibble_row(!!!default_values)))
+}
+
+expand2 <- function(.data, ..., .default = NULL, .order = FALSE) {
+  quos <- enquos(...)
+  stopifnot(rlang::is_bool(.order))
+
+  if (!is.null(.default)) {
+    quos <- c(quos, expand_defaults(.data, quos, .default))
+  }
+
+  out <- tidyr::expand(data, !!!quos)
+
+  if (.order) {
+    out <- dplyr::select(out, !!!names(.data), !!!setdiff(names(out), names(.data)))
+  }
+
+  out
+}
+
+expand3 <- function(.data, ..., .filter = NULL) {
+  filter_quo <- enquo(.filter)
+
+  out <- expand2(.data, ...)
+
+  if (!quo_is_null(filter_quo)) {
+    out <- dplyr::filter(out, !!filter_quo)
+  }
+
+  out
+}
+
+auto_expand <- function(.data, ..., .filter = NULL) {
+  expand3(.data, ..., .filter = {{ .filter }}, .default = new_value, .order = TRUE)
+}
--- R/newdata-package.R
+++ R/newdata-package.R
@@ -4,12 +4,19 @@
 ## usethis namespace: start
 #' @import chk
 #' @importFrom hms as_hms
+#' @importFrom rlang :=
 #' @importFrom rlang %||%
+#' @importFrom rlang enquo
+#' @importFrom rlang enquos
+#' @importFrom rlang ensym
 #' @importFrom rlang global_env
 #' @importFrom rlang inject
 #' @importFrom rlang is_logical
 #' @importFrom rlang is_named has_name as_function set_names
 #' @importFrom rlang is_true
+#' @importFrom rlang quo
+#' @importFrom rlang quo_is_null
 #' @importFrom tibble %>% as_tibble
+#' @importFrom utils data
 ## usethis namespace: end
 NULL
