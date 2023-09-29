28e2de80cf2803fac11b5c185799be0adab459fc
Kirill Müller <kirill@cynkra.com>
2023-09-29 15:44:00 -0700

    Move xobs_only tools

--- R/xutils.R
+++ R/xutils.R
@@ -24,27 +24,3 @@ expand2 <- function(.data, ..., .default = NULL, .order = FALSE) {
 
   out
 }
-
-semi_crossing <- function(..., .data = xnew_data_env$data) {
-  if (is.null(.data)) {
-    err("`semi_crossing()` can only be called from `xnew_data()`")
-  }
-
-  out <- tidyr::crossing(...)
-  dplyr::semi_join(out, .data, by = intersect(names(out), names(.data)))
-}
-
-quo_translate_xobs_only <- function(quo, length_out) {
-  new_quosure(
-    expr_translate_xobs_only(quo_get_expr(quo), length_out),
-    quo_get_env(quo)
-  )
-}
-
-expr_translate_xobs_only <- function(expr, length_out) {
-  if (is_symbol(expr)) {
-    expr(xnew_seq(!!expr, length_out = !!length_out, obs_only = TRUE))
-  } else {
-    expr
-  }
-}
