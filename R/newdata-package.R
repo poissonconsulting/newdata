f5684d8806885d15ae664feb7c33ba765a7bccbd
Kirill Müller <kirill@cynkra.com>
2023-09-29 12:49:31 -0700

    xobs_only()

--- R/newdata-package.R
+++ R/newdata-package.R
@@ -9,12 +9,20 @@
 #' @importFrom rlang enquo
 #' @importFrom rlang enquos
 #' @importFrom rlang ensym
+#' @importFrom rlang expr
 #' @importFrom rlang global_env
 #' @importFrom rlang inject
 #' @importFrom rlang is_logical
 #' @importFrom rlang is_named has_name as_function set_names
+#' @importFrom rlang is_symbol
+#' @importFrom rlang is_syntactic_literal
 #' @importFrom rlang is_true
+#' @importFrom rlang local_bindings
+#' @importFrom rlang new_environment
+#' @importFrom rlang new_quosure
 #' @importFrom rlang quo
+#' @importFrom rlang quo_get_env
+#' @importFrom rlang quo_get_expr
 #' @importFrom rlang quo_is_null
 #' @importFrom tibble %>% as_tibble
 #' @importFrom utils data
--- R/xnew-data.R
+++ R/xnew-data.R
@@ -37,6 +37,45 @@ expand2 <- function(.data, ..., .default = NULL, .order = FALSE) {
   out
 }
 
+xnew_data_env <- new_environment()
+
 xnew_data <- function(.data, ...) {
+  stopifnot(is.null(xnew_data_env$data))
+  local_bindings(data = .data, .env = xnew_data_env)
+
   expand2(.data, ..., .default = new_value, .order = TRUE)
+}
+
+#' @export
+xobs_only <- function(..., .length_out = NULL) {
+  quos <- enquos(...)
+
+  translated <- map(quos, quo_translate_xobs_only, .length_out)
+
+  semi_crossing(!!!translated)
+}
+
+# @export, maybe?
+semi_crossing <- function(..., .data = xnew_data_env$data) {
+  if (is.null(.data)) {
+    err("`semi_crossing()` can only be called from `xnew_data()`")
+  }
+
+  out <- tidyr::crossing(...)
+  dplyr::semi_join(out, .data, by = intersect(names(out), names(.data)))
+}
+
+quo_translate_xobs_only <- function(quo, length_out) {
+  new_quosure(
+    expr_translate_xobs_only(quo_get_expr(quo), length_out),
+    quo_get_env(quo)
+  )
+}
+
+expr_translate_xobs_only <- function(expr, length_out) {
+  if (is_symbol(expr)) {
+    expr(xnew_seq(!!expr, length_out = !!length_out, obs_only = TRUE))
+  } else {
+    expr
+  }
 }
