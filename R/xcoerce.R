96aae06efe7e0442f2f8921f7fb3ef4aeac3c3a4
Kirill Müller <kirill@cynkra.com>
2023-10-18 18:44:30 +0200

    Draft: xcoerce()

---
+++ R/xcoerce.R
@@ -0,0 +1,16 @@
+#' Coerce
+#'
+#' @param ... One or more variables to generate combinations for.
+#' @param .data TBD
+#' @seealso [xnew_data()], [xnew_value()] and [xnew_seq()]
+#' @export
+#' @examples
+#' # TBD
+xcoerce <- function(..., .data = xnew_data_env$data) {
+  values <- list2(...)
+  stopifnot(all(names2(values) != ""))
+
+  stopifnot(all(names(values) %in% names(.data)))
+
+  vec_cast(values, .data[names(values)])
+}
