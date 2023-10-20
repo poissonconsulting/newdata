17cc7f0001eba1d12dff3d15440d588948f0da93
Joe Thorley <joe@poissonconsulting.ca>
2023-10-20 14:35:57 -0700

    more tests

--- tests/testthat/test-xcoerce.R
+++ tests/testthat/test-xcoerce.R
@@ -19,6 +19,7 @@ test_that("simple dataset", {
     xnew_data(data, xcoerce(a = NA))
     xnew_data(data, xcoerce(a = numeric())) # FIXME: should this be an error?
     xnew_data(data, xcoerce(a = 1, b = 6))
+    expect_error(xnew_data(data, xcoerce(a = 1, b = 6.5)))
     xnew_data(data, xcoerce(a = 1, b = 6, c = "e"))
     expect_error(xnew_data(data, xcoerce(c = "f")))
     xnew_data(data, xcoerce(c = c("b", "a"))) # FIXME: should this be unsorted?
@@ -26,6 +27,9 @@ test_that("simple dataset", {
     xnew_data(data, xcoerce(a = 1, b = 6, c = factor("a", levels = "a")))$c
     xnew_data(data, xobs_only(xcoerce(b = 2:3)))
     xnew_data(data, xobs_only(xcoerce(b = new_value(b))))
+    xnew_data(data, xobs_only(xcoerce(b = new_seq(b))))
+    xnew_data(data, xcoerce(a = new_seq(a)))
+    xnew_data(data, xobs_only(xcoerce(a = new_seq(a))))
     xnew_data(data, xobs_only(xcoerce(b = 2L))) # FIXME: should this be an error
     xnew_data(data, xobs_only(xcoerce(b = 1:3)), xobs_only(c = c("a", "b")))
     xnew_data(data, xobs_only(xcoerce(b = 1:4), c = c("a", "b", "c")))
