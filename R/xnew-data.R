f1abee445161bab91cfe0683a97934b63ca14d58
Joe Thorley <joe@poissonconsulting.ca>
2026-09-08 11:44:03 -0700

    Revert "Update R/xnew-data.R"
    This reverts commit 596ee3e7c551c2214e963a1f9501558da75d9266.

--- R/xnew-data.R
+++ R/xnew-data.R
@@ -82,7 +82,7 @@ quo_translate_xnew_data <- function(quo, name, length_out) {
   }
   # the user's quosure is nested inside one evaluated in the package namespace
   # so that the internal xnew_column() is in scope
-  expr(!!xnew_column(quo, name))
+  new_quosure(expr(xnew_column(!!quo, !!name)), ns_env("newdata"))
 }
 
 # A named argument must reach tidyr::expand() as a one column data frame.
